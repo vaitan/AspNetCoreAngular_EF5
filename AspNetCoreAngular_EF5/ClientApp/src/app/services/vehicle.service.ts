c75d4c8de6866faac67299de37b16db618f7e0e2
vaitan <tai.nguyenvan.dev@gmail.com>
2021-09-26 19:15:09 +0700

    Submit form

--- AspNetCoreAngular_EF5/ClientApp/src/app/services/vehicle.service.ts
+++ AspNetCoreAngular_EF5/ClientApp/src/app/services/vehicle.service.ts
@@ -20,4 +20,9 @@ export class VehicleService {
     return this.http.get<any[]>('/api/features')
       .pipe(map(res => res));
   }
+
+  create(vehicle) {
+    return this.http.post<any>('/api/vehicles', vehicle)
+      .pipe(map(res => res));
+  }
 }
--- AspNetCoreAngular_EF5/ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ AspNetCoreAngular_EF5/ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -10,7 +10,10 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   features: any[];
-  vehicle: any = {};
+  vehicle: any = {
+    features: [],
+    contact: {}
+  };
 
   constructor(private vehicleService: VehicleService) { }
 
@@ -25,7 +28,24 @@ export class VehicleFormComponent implements OnInit {
   }
 
   onMakeChange() {
-    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
+    delete this.vehicle.modelId;
+  }
+
+  onFeaturesToggled(featureId, $event) {
+    if ($event.target.checked) {
+      this.vehicle.features.push(featureId);
+    }
+    else {
+      let index = this.vehicle.features.indexOf(featureId);
+      this.vehicle.features.splice(index, 1);
+    }
+  }
+
+  submit() {
+    this.vehicle.isRegistered = this.vehicle.isRegistered == "true";
+    this.vehicleService.create(this.vehicle)
+      .subscribe(x => console.log(x));
   }
 }
